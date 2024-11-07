cfbb78915fcc8ce8027dc01e2f020950be0459b0
emmat <emma.trowbridgee@gmail.com>
2024-11-07 13:53:00 -0600

    Final Commit Chapter 9

---
+++ app/src/main/java/com/bignerdranch/android/chapter_nine/CrimeDetailFragment.kt
@@ -0,0 +1,60 @@
+package com.bignerdranch.android.chapter_nine
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import com.bignerdranch.android.chapter_nine.databinding.ActivityMainBinding
+import com.bignerdranch.android.chapter_nine.databinding.FragmentCrimeDetailBinding
+import java.util.Date
+import java.util.UUID
+
+private lateinit var binding: FragmentCrimeDetailBinding
+
+
+class CrimeDetailFragment : Fragment() {
+
+    var crime: Crime = Crime(
+        id = UUID.randomUUID(),
+        title = "",
+        date = Date(),
+        isSolved = false
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentCrimeDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            crimeTitle.doOnTextChanged { text, _, _, _ ->
+                crime = crime.copy(title = text.toString())
+            }
+
+
+            crimeDate.apply {
+                text = crime.date.toString()
+                isEnabled = false
+            }
+
+            crimeSolved.setOnCheckedChangeListener { _, isChecked ->
+                crime = crime.copy(isSolved = isChecked)
+
+            }
+        }
+    }
+}
